24e634c4e9c7a2da6b26e65df02e703e29b580c8
Kristof Maar <kristof.maar@gmail.com>
2024-01-22 20:06:47 +0100

    Update prepare integrals for test

--- code/x_prepare_integrals_for_test.py
+++ code/x_prepare_integrals_for_test.py
@@ -1,20 +1,16 @@
 import os
 from PIL import Image
 
-# Source and destination folder paths
 source_folder = 'integrals'
-base_output_folder = 'dataset_pix2pix_test/input/test'
+base_output_folder = 'train_test'
 
-# Create the base output folder if it doesn't exist
 if not os.path.exists(base_output_folder):
     os.makedirs(base_output_folder)
 
-# Dictionary to track images by number
 image_files = {}
 
-# Iterate through files and group them by number
 for filename in os.listdir(source_folder):
-    if filename.startswith("1_") and "_integral_f-" in filename:
+    if filename.startswith("0_") and "_integral_f-" in filename:
         parts = filename.split('_')
         number = parts[1]
         focal_length = parts[3].replace("f-", "").replace(".png", "")
@@ -28,10 +24,8 @@ for filename in os.listdir(source_folder):
             elif focal_length == "1.6":
                 image_files[number]['D'] = os.path.join(source_folder, filename)
 
-# Function to concatenate and save images
 def concatenate_and_save_images():
     for number, images in image_files.items():
-        # Load images if they exist and concatenate them horizontally
         imgs_to_concat = [Image.open(images[fl]) for fl in ['B', 'C', 'D'] if images[fl] is not None]
         if len(imgs_to_concat) == 3:
             concatenated_img = Image.new('RGB', (sum(img.width for img in imgs_to_concat), imgs_to_concat[0].height))
@@ -39,8 +33,6 @@ def concatenate_and_save_images():
             for img in imgs_to_concat:
                 concatenated_img.paste(img, (x_offset, 0))
                 x_offset += img.width
-            # Save the concatenated image
             concatenated_img.save(os.path.join(base_output_folder, f"{number}.png"))
 
-# Execute the function
 concatenate_and_save_images()
